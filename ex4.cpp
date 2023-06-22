f99cdf33833ebff816caef8c493b90b8c31e2949
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-06-22 17:19:21 +0300

    Create ex4.cpp
    Add the solution of the exercise

---
+++ ex4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int month;
+    cout<<"Type number of month: ";
+    cin >> month;
+    switch(month) {
+        case 1:
+        cout << "January - 31 days" << endl;
+        break;
+        case 2:
+        cout << "February - 28 days " << endl;
+        break;
+        case 3:
+        cout << "March - 31 days " << endl;
+        break;
+        case 4:
+        cout << "April - 30 days " << endl;
+        break;
+        case 5:
+        cout << "May - 31 days " << endl;
+        break;
+        case 6:
+        cout << "June - 30 days " << endl;
+        break;
+        case 7:
+        cout << "July - 31 days " << endl;
+        break;
+        case 8:
+        cout << "August - 31 days " << endl;
+        break;
+        case 9:
+        cout << "September - 30 days " << endl;
+        break;
+        case 10:
+        cout << "October - 31 days " << endl;
+        break;
+        case 11:
+        cout << "November - 30 days " << endl;
+        break;
+        case 12:
+        cout << "December - 31 days " << endl;
+        break;
+        
+    }
+    
+    
+
+    return 0;
+}
